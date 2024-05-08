3e8172ce1143b9040d5f937075a1f05b94bf9e8d
nolancrandall <51757666+nolancrandall@users.noreply.github.com>
2024-05-07 21:33:11 -0400

    Update and rename Mario.ipymb to Mario.py

--- Mario/Mario.py
+++ Mario/Mario.py
@@ -1,48 +1,34 @@
-. Setup Mario
-!pip install gym_super_mario_bros==7.3.0 nes_py
-# Import the game
 import gym_super_mario_bros
-# Import the Joypad wrapper
+
 from nes_py.wrappers import JoypadSpace
-# Import the SIMPLIFIED controls
+
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-# Setup game
+
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-# Create a flag - restart or not
-done = True
-# Loop through each frame in the game
+
+
 for step in range(100000): 
-    # Start the game to begin with 
     if done: 
-        # Start the gamee
         env.reset()
-    # Do random actions
     state, reward, done, info = env.step(env.action_space.sample())
-    # Show the game on the screen
     env.render()
-# Close the game
 env.close()
-2. Preprocess Environment
-# Install pytorch
-!pip install torch==1.10.1+cu113 torchvision==0.11.2+cu113 torchaudio===0.10.1+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
-# Install stable baselines for RL stuff
-!pip install stable-baselines3[extra]
-# Import Frame Stacker Wrapper and GrayScaling Wrapper
+
 from gym.wrappers import GrayScaleObservation
-# Import Vectorization Wrappers
+
 from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
-# Import Matplotlib to show the impact of frame stacking
+
 from matplotlib import pyplot as plt
-# 1. Create the base environment
+
 env = gym_super_mario_bros.make('SuperMarioBros-v0')
-# 2. Simplify the controls 
+
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-# 3. Grayscale
+
 env = GrayScaleObservation(env, keep_dim=True)
-# 4. Wrap inside the Dummy Environment
+
 env = DummyVecEnv([lambda: env])
-# 5. Stack the frames
+
 env = VecFrameStack(env, 4, channels_order='last')
 state = env.reset()
 state, reward, done, info = env.step([5])
@@ -51,12 +37,15 @@ for idx in range(state.shape[3]):
     plt.subplot(1,4,idx+1)
     plt.imshow(state[0][:,:,idx])
 plt.show()
-3. Train the RL Model
-# Import os for file path management
+
+
+
+
+#Trains the model
 import os 
-# Import PPO for algos
+
 from stable_baselines3 import PPO
-# Import Base Callback for saving models
+
 from stable_baselines3.common.callbacks import BaseCallback
 class TrainAndLoggingCallback(BaseCallback):
 
@@ -75,23 +64,21 @@ class TrainAndLoggingCallback(BaseCallback):
             self.model.save(model_path)
 
         return True
-CHECKPOINT_DIR = './train/'
-LOG_DIR = './logs/'
-# Setup model saving callback
+
 callback = TrainAndLoggingCallback(check_freq=10000, save_path=CHECKPOINT_DIR)
-# This is the AI model started
+
 model = PPO('CnnPolicy', env, verbose=1, tensorboard_log=LOG_DIR, learning_rate=0.000001, 
             n_steps=512) 
-# Train the AI model, this is where the AI model starts to learn
+
 model.learn(total_timesteps=1000000, callback=callback)
 model.save('thisisatestmodel')
-4. Test it Out
-# Load model
+
+
 model = PPO.load('./train/best_model_1000000')
 state = env.reset()
-# Start the game 
+
 state = env.reset()
-# Loop through the game
+
 while True: 
     
     action, _ = model.predict(state)
